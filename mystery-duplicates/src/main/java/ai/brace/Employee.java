00ad856ba765018e0ca484e0da6899ac118f8a71
Scott MacDonell <scott@acadaca.com>
2020-09-16 18:16:33 -0400

    Scott MacDonell - Brace Interview Commit: Committing all updates for mystery-duplicates.

--- mystery-duplicates/src/main/java/ai/brace/Employee.java
+++ mystery-duplicates/src/main/java/ai/brace/Employee.java
@@ -14,4 +14,27 @@ public class Employee
         lastName = _lastName;
         socialSecurityNumber = _socialSecurityNumber;
     }
+
+    /**
+     * Make sure equals() is overridden with string values.
+     *
+     * @param obj
+     * @return
+     */
+    @Override
+    public boolean equals(Object obj) {
+        Employee e = (Employee) obj;
+        return (this.firstName.equals(e.firstName) && this.middleInitial.equals(e.middleInitial)
+                && this.lastName.equals(e.lastName) && this.socialSecurityNumber.equals(e.socialSecurityNumber));
+    }
+
+    /**
+     * Adding override for hashCode() to make sure hashCode is generated based on the string field values.
+     * @return
+     */
+    @Override
+    public int hashCode() {
+        return 1 * (this.firstName.hashCode() + this.middleInitial.hashCode()
+                + this.lastName.hashCode() + this.socialSecurityNumber.hashCode());
+    }
 }
